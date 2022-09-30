0bd2f3e254987dfb5508b8f0a5e9dc93e240518d
Nick.Hsu <Nick.Hsu@testritegroup.com>
2022-09-30 16:22:37 +0800

    Sec 15. 200. TD: Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,44 @@ export class AppComponent {
 
   defaultQuestion = 'pet';
 
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
+  // onSubmit() {
+  //   console.log(this.signupForm);
+  // }
+
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
